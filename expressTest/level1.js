ca2cbbf1a0ac52fc0c1e96a3fc47ba9502a3253a
Michael DAntuono <mikeishere3@gmail.com>
2018-01-27 17:45:07 +0000

    level 1 express

--- expressTest/level1.js
+++ expressTest/level1.js
@@ -4,7 +4,15 @@ var app = express();
 app.get('/', function(request, response) {
     response.send('Hello world');
 });
-
+app.get('/name', function(request, response) {
+    response.send("Michael DAntuono");
+});
+app.get('/redirect', function(request, response) {
+    
+});
+app.get('/date', function(request, response) {
+   response.send(Date.now());
+});
 
 app.listen(process.env.PORT, function() {
     console.log("Listening on port 3000");
