8060541279c39224a462684179bcbc0c96e42411
Michael DAntuono <mikeishere3@gmail.com>
2018-02-05 15:38:44 +0000

    level 2 started

--- expressTest/app.js
+++ expressTest/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cities = ["Providence", "Miami", "San Diego"];
 
 app.get('/', function(request, response) {
     response.send('Hello world');
@@ -11,11 +12,15 @@ app.get('/redirect', function(request, response) {
     response.redirect(301, '/surprise');
 });
 app.get('/surprise', function(request, response) {
-    response.send("Get redirected!")
+    response.send("Get redirected!");
 });
 app.get('/date', function(request, response) {
    response.send(new Date());
 });
+app.get('/cities', function(request, response) {
+    response.send(cities);
+});
+app.use();
 
 app.listen(process.env.PORT, function() {
     console.log("Listening on port 3000");
