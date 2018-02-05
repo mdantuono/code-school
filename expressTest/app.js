8b97583a048f2c59fcdd773f70cb85cadb78548f
Michael DAntuono <mikeishere3@gmail.com>
2018-02-05 16:46:30 +0000

    level 2

--- expressTest/app.js
+++ expressTest/app.js
@@ -2,9 +2,8 @@ var express = require('express');
 var app = express();
 var cities = ["Providence", "Miami", "San Diego"];
 
-app.get('/', function(request, response) {
-    response.send('Hello world');
-});
+app.use(express.static('public'));
+
 app.get('/name', function(request, response) {
     response.send("Michael DAntuono");
 });
@@ -20,7 +19,6 @@ app.get('/date', function(request, response) {
 app.get('/cities', function(request, response) {
     response.send(cities);
 });
-app.use();
 
 app.listen(process.env.PORT, function() {
     console.log("Listening on port 3000");
