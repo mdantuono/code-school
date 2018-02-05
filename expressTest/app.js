37421487919a5b9856caf5f9cc64b4d9e848d4a3
Michael DAntuono <mikeishere3@gmail.com>
2018-02-05 18:08:00 +0000

    level 2 done

--- expressTest/app.js
+++ expressTest/app.js
@@ -1,25 +1,15 @@
 var express = require('express');
 var app = express();
-var cities = ["Providence", "Miami", "San Diego"];
+var cities = ["Providence",
+              "Miami",
+              "San Diego"];
 
 app.use(express.static('public'));
 
-app.get('/name', function(request, response) {
-    response.send("Michael DAntuono");
-});
-app.get('/redirect', function(request, response) {
-    response.redirect(301, '/surprise');
-});
-app.get('/surprise', function(request, response) {
-    response.send("Get redirected!");
-});
-app.get('/date', function(request, response) {
-   response.send(new Date());
-});
 app.get('/cities', function(request, response) {
     response.send(cities);
 });
 
 app.listen(process.env.PORT, function() {
-    console.log("Listening on port 3000");
+    console.log("Listening on port FUCKYOU");
 });
