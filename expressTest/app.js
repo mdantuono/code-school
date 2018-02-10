eeef8a8fb4f341084074304e761e2efa2368c780
Michael DAntuono <mikeishere3@gmail.com>
2018-02-10 17:31:38 +0000

    level 3 done

--- expressTest/app.js
+++ expressTest/app.js
@@ -2,16 +2,22 @@ var express = require('express');
 var app = express();
 var cities = ["Providence",
               "Miami",
-              "San Diego"];
+              "San Diego",
+              "Philidelphia",
+              "Chicago"];
 
 app.use(express.static('public'));
 
 app.get('/cities', function(request, response) {
-    response.send(cities);
+    if (request.query.limit >= 0) {
+        response.json(cities.slice(0, request.query.limit));
+    } else if (request.query.limit > cities.length) {
+        response.status(400).json('Requested limit too large');
+    } else {
+        response.send(cities);
+    }
 });
-app.get('/cities', function(request, response) {
-   response.send(["Providence", "San Diego", "Miami"]); 
-});
+
 app.listen(process.env.PORT, function() {
-    console.log("Listening on port FUCKYOU");
+    console.log("Listening on port 3000");
 });
