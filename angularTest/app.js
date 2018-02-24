aeb4a6a50964509d305ff0c32806408c21fe5d0e
Michael DAntuono <mikeishere3@gmail.com>
2018-02-24 18:18:51 +0000

    html complete, finishing js file

--- angularTest/app.js
+++ angularTest/app.js
@@ -8,19 +8,25 @@ var cars = [
         {
             name: "Ferrari",
             price: 250,
-            image: "Ferrari.jpg",
-            description: "Is really fast car."
+            image: "images/Ferrari.jpg",
+            description: "Is really fast car.",
+            specs: "700 hp",
+            quantity: 10
         },
         {
             name: "Lamborghini",
             price: 500,
-            image: "Lamborghini.jpg",
-            description: "Is very faster car."
+            image: "images/Lamborghini.jpg",
+            description: "Is very faster car.",
+            specs: "720 hp",
+            quantity: 10
         },
         {
             name: "Bugatti",
             price: 750,
-            image: "Bugatti.jpeg",
-            description: "Car melts face off."
+            image: "images/Bugatti.jpeg",
+            description: "Car melts face off.",
+            specs: "1500 hp",
+            quantity: 10
         }
-];
+]; 
